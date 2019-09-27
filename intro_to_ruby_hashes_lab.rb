933822e27fb8696499d0ed0415e7981718390616
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-27 15:48:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
-				:syme => "4"
+				:four_pieces_owned => 200
 			},
 			:names => {
 				:hi => "1",
