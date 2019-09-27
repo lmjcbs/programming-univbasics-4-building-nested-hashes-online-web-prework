2588f3cab696fcbc37ddf00ba2e79f1a1dbb064f
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-27 15:43:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:rent_in_dollars => {
-				:1,
-				:2,
-				:3,
-				:4
+				:1 => "1",
+				:2 => "2",
+				:3 => "3",
+				:4 => "4"
 			}
 		}
 	}
