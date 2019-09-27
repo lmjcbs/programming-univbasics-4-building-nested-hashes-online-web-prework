33ee7bbb457fb0cf5789000f726220b35b28bf11
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-27 15:47:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
 		:railroads => {
 			:pieces => 4,
 			:rent_in_dollars => {
-				:one_piece_owned => "1",
+				:one_piece_owned => "25",
 				:sym => "2",
 				:symee => "3",
 				:syme => "4"
